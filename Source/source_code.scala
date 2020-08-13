903aac3074c46431ee5daf7991f5013de198288f
Ravi Chaubey <ravichaubey43@gmail.com>
2020-08-13 18:22:06 +0530

    Update source_code.scala

--- Source/source_code.scala
+++ Source/source_code.scala
@@ -6,18 +6,8 @@ val ranks = graph.pageRank(0.0001).vertices
 
 ranks.collect
 
-val users = sc.textFile("/user/ravichaubey43_gmail/users.txt").map { line =>
-     | 
-     |   val fields = line.split(",")
-     | 
-     |   (fields(0).toLong, fields(1))
-     | 
-     | }
+val users = sc.textFile("/user/ravichaubey43_gmail/users.txt").map { line => val fields = line.split(",") (fields(0).toLong, fields(1))}
      
-val ranksByUsername = users.join(ranks).map {
-     | 
-     |   case (id, (username, rank)) => (username, rank)
-     | 
-     | }
-     
+val ranksByUsername = users.join(ranks).map {case (id, (username, rank)) => (username, rank)}
+
 println(ranksByUsername.collect().mkString("\n"))
